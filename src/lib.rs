8abe47ad713d1fdf335c5814dcefd173a871b094
qiaoruntao <qiaoruntao01@gmail.com>
2021-07-31 03:55:23 +0800

    Re-export macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// re-export macros
+#[cfg(feature = "qrt_rust_macros")]
+#[allow(unused_imports)]
+#[macro_use]
+extern crate qrt_rust_macros;
+
+#[cfg(feature = "qrt_rust_macros")]
+#[doc(hidden)]
+pub use qrt_rust_macros::*;
+
 pub mod config_manage;
 pub mod mongodb_manager;
 pub mod request_utils;
