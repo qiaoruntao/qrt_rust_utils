13a79beb768d9ba904a2d1e7e921781693805880
qiaoruntao <925783095@qq.com>
2021-08-01 01:07:20 +0800

    Support serde rename

--- qrt_rust_macros/src/lib.rs
+++ qrt_rust_macros/src/lib.rs
@@ -3,9 +3,9 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 use quote::quote;
-use syn::{Data, DeriveInput, Ident};
+use syn::{Data, DeriveInput, Fields, Ident, NestedMeta};
 
-#[proc_macro_derive(ToHashMap)]
+#[proc_macro_derive(ToHashMap, attributes(serde))]
 pub fn derive_to_hashmap(input_struct: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input_struct as DeriveInput);
 
@@ -14,6 +14,47 @@ pub fn derive_to_hashmap(input_struct: TokenStream) -> TokenStream {
         Data::Struct(st) => st.fields,
         _ => panic!("Implementation must be a struct"),
     };
+    let mut rename_map = std::collections::HashMap::new();
+    match fields {
+        Fields::Named(_) => {
+            for field in fields.iter() {
+                for attr in field.attrs.iter() {
+                    let field_name = field.ident.as_ref().unwrap().to_string();
+                    let is_already_defined = rename_map.contains_key(&field_name);
+                    if is_already_defined {
+                        panic!("Field name {} is already defined", &field_name);
+                    }
+                    match attr.parse_meta() {
+                        Ok(syn::Meta::List(lst)) => {
+                            for sub_attr in lst.nested.iter() {
+                                match sub_attr {
+                                    NestedMeta::Meta(syn::Meta::NameValue(name_value)) => {
+                                        let sub_attr_name = name_value.path.get_ident().unwrap().to_string();
+                                        if sub_attr_name != "rename" {
+                                            break;
+                                        }
+                                        let alternative_name = match &name_value.lit {
+                                            syn::Lit::Str(value) => value.value(),
+                                            _ => { break; }
+                                        };
+                                        rename_map.insert(field_name, alternative_name);
+                                        // there should only be one rename, ignore other renames
+                                        break;
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+        _ => {
+            panic!("All fields must be named in struct");
+        }
+    }
+
 
     // parse out all the field names in the struct as `Ident`s
     let idents: Vec<&Ident> = fields
@@ -26,6 +67,7 @@ pub fn derive_to_hashmap(input_struct: TokenStream) -> TokenStream {
         .clone()
         .iter()
         .map(|ident| ident.to_string())
+        .map(|ident| rename_map.get(&ident).unwrap_or(&ident).clone())
         .collect::<Vec<String>>();
 
     // get the name identifier of the struct input AST
--- tests/test.rs
+++ tests/test.rs
@@ -1,16 +1,14 @@
-use qrt_rust_macros::ToHashMap;
-
-#[derive(ToHashMap)]
-struct TestStruct {
-    name: String,
-    number: u32,
-}
-
 #[cfg(test)]
 mod test_basic {
     use std::collections::HashMap;
 
-    use crate::TestStruct;
+    use qrt_rust_macros::ToHashMap;
+
+    #[derive(ToHashMap)]
+    struct TestStruct {
+        name: String,
+        number: u32,
+    }
 
     #[test]
     fn test_into() {
@@ -21,3 +19,25 @@ mod test_basic {
     }
 }
 
+#[cfg(test)]
+mod test_serde_basic {
+    use std::collections::HashMap;
+
+    use qrt_rust_macros::ToHashMap;
+
+    #[derive(ToHashMap)]
+    struct TestStruct2 {
+        #[serde(rename = "name2")]
+        name: String,
+        number: u32,
+    }
+
+    #[test]
+    fn test() {
+        let test_struct = TestStruct2 { name: "".to_string(), number: 123 };
+        let x: HashMap<String, String> = test_struct.into();
+        assert_eq!(x.get("name"), None);
+        assert_eq!(x.get("name2").unwrap().as_str(), "");
+        assert_eq!(x.get("number").unwrap().as_str(), "123");
+    }
+}
