64ef2f6ca7a85ff00bc28ec88344606306699e4f
qiaoruntao <qiaoruntao01@gmail.com>
2021-08-02 22:39:38 +0800

    Add duration

--- src/set_interval/set_interval.rs
+++ src/set_interval/set_interval.rs
@@ -4,34 +4,36 @@ use std::time::Duration;
 use async_recursion::async_recursion;
 
 #[async_recursion]
-async fn set_interval_inner<F, K>(func: F) -> ()
+async fn set_interval_inner<F, K>(func: F, duration: Duration) -> ()
     where F: Fn() -> K + Send + 'static + Sync {
-    println!("set_interval_inner");
-    tokio::time::sleep(Duration::from_millis(100)).await;
+    // println!("set_interval_inner");
+    tokio::time::sleep(duration).await;
     func();
-    set_interval_inner(func).await;
+    set_interval_inner(func, duration).await;
 }
 
 #[async_recursion]
-async fn set_interval_async_inner<F, T>(func: F) -> ()
+async fn set_interval_async_inner<F, T>(func: F, duration: Duration) -> ()
     where F: Fn() -> T + Send + 'static + Sync, T: Future + Send + 'static {
-    println!("set_interval_async_inner");
-    tokio::time::sleep(Duration::from_millis(100)).await;
+    // println!("set_interval_async_inner");
+    tokio::time::sleep(duration).await;
     func().await;
-    set_interval_async_inner(func).await;
+    set_interval_async_inner(func, duration).await;
 }
 
-pub fn set_interval<F, K>(func: F) -> ()
+/// sync version of set_interval
+pub fn set_interval<F, K>(func: F, duration: Duration) -> ()
     where F: (Fn() -> K) + Send + 'static + Sync {
     tokio::spawn(async move {
-        set_interval_inner(func).await;
+        set_interval_inner(func, duration).await;
     });
 }
 
-pub fn set_interval_async<F, T>(func: F) -> ()
+/// async version of set_interval
+pub fn set_interval_async<F, T>(func: F, duration: Duration) -> ()
     where F: (Fn() -> T) + Send + 'static + Sync, T: Future + Send + 'static {
     tokio::spawn(async move {
-        set_interval_async_inner(func).await;
+        set_interval_async_inner(func, duration).await;
     });
 }
 
@@ -52,10 +54,11 @@ mod test_set_interval {
 
     #[tokio::test]
     async fn test() {
+        let duration = Duration::from_millis(100);
         set_interval(|| {
             println!("sync {}", Local::now());
-        });
-        set_interval_async(fn_async);
+        }, duration);
+        set_interval_async(fn_async, duration);
         tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
