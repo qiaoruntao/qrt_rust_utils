5f9307e3ca68d5cec6db152fb602258ae38a84d6
qiaoruntao <qiaoruntao01@gmail.com>
2021-08-02 23:29:21 +0800

    Implement set_interval for tokio Runtime

--- src/set_interval/set_interval.rs
+++ src/set_interval/set_interval.rs
@@ -21,6 +21,34 @@ async fn set_interval_async_inner<F, T>(func: F, duration: Duration) -> ()
     set_interval_async_inner(func, duration).await;
 }
 
+pub trait SetInterval {
+    //noinspection RsSelfConvention
+    fn set_interval<F, K>(&self, func: F, duration: Duration) -> ()
+        where F: (Fn() -> K) + Send + 'static + Sync;
+    //noinspection RsSelfConvention
+    fn set_interval_async<F, T>(&self, func: F, duration: Duration) -> ()
+        where F: (Fn() -> T) + Send + 'static + Sync, T: Future + Send + 'static;
+}
+
+impl SetInterval for tokio::runtime::Runtime {
+    /// sync version of set_interval
+    fn set_interval<F, K>(&self, func: F, duration: Duration) -> ()
+        where F: (Fn() -> K) + Send + 'static + Sync {
+        self.spawn(async move {
+            set_interval_inner(func, duration).await;
+        });
+    }
+
+    /// async version of set_interval
+    fn set_interval_async<F, T>(&self, func: F, duration: Duration) -> ()
+        where F: (Fn() -> T) + Send + 'static + Sync, T: Future + Send + 'static {
+        self.spawn(async move {
+            set_interval_async_inner(func, duration).await;
+        });
+    }
+}
+
+// TODO: find a way to deduplicate these parts
 /// sync version of set_interval
 pub fn set_interval<F, K>(func: F, duration: Duration) -> ()
     where F: (Fn() -> K) + Send + 'static + Sync {
@@ -43,7 +71,7 @@ mod test_set_interval {
 
     use chrono::Local;
 
-    use crate::set_interval::set_interval::{set_interval, set_interval_async};
+    use crate::set_interval::set_interval::{set_interval, set_interval_async, SetInterval};
 
     async fn fn_async() {
         println!("fn_async");
@@ -60,5 +88,20 @@ mod test_set_interval {
         }, duration);
         set_interval_async(fn_async, duration);
         tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+
+    #[test]
+    fn test_runtime() {
+        let runtime = tokio::runtime::Runtime::new().unwrap();
+        let duration = Duration::from_millis(100);
+        runtime.set_interval(|| {
+            println!("sync {}", Local::now());
+        }, duration);
+        runtime.set_interval_async(fn_async, duration);
+
+        // wait for 1 second
+        runtime.block_on(async {
+            tokio::time::sleep(Duration::from_secs(1)).await
+        })
     }
 }
