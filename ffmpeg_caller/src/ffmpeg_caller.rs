45f9c3c0a618312e0645988427215f9e1f07ea64
qiaoruntao <qiaoruntao01@gmail.com>
2022-12-03 01:50:44 +0800

    Fix bug

--- ffmpeg_caller/src/ffmpeg_caller.rs
+++ ffmpeg_caller/src/ffmpeg_caller.rs
@@ -92,7 +92,8 @@ impl FfmpegCaller {
         let mut command = Command::new("ffmpeg");
         let command = command
             .stdin(Stdio::null())
-            .stderr(Stdio::piped())
+            // .stderr(Stdio::piped())
+            .stderr(Stdio::null())
             .stdout(Stdio::null())
             .kill_on_drop(true)
             .args(args)
