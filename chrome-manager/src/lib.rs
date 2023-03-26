c658d99685f035f722c0636ae02cedeec17acff6
qiaoruntao <qiaoruntao@qq.com>
2023-03-27 03:25:11 +0800

    Init ChromeManager

---
+++ chrome-manager/src/lib.rs
@@ -0,0 +1,13 @@
+pub struct ChromeManager {}
+
+impl ChromeManager {
+    pub fn connect<S: AsRef<str>>(endpoint: S) -> ChromeManager {
+        ChromeManager{
+
+        }
+    }
+
+    pub fn get_session<S: AsRef<str>>(key:S){
+
+    }
+}
